c59cdaa835b53a1f76f1cf58ee8b1bd7cf4c29d9
Javier Sanchez <javier.sanchez@mexicoder.com.mx>
2015-07-12 18:43:29 +0000

    Pagination for wikipedia and twitter results added

--- kabbage/common/views.py
+++ kabbage/common/views.py
@@ -2,6 +2,7 @@ from requests import ConnectionError
 import twython
 import wikipedia
 from django.conf import settings
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import FormView, TemplateView
 from .forms import SearchForm
 
@@ -18,38 +19,60 @@ class SearchFormView(FormView):
     error_message = 'There was an error in the search'
 
     def get(self, request, *args, **kwargs):
+        self.context = {}
+        self.page_twitter = request.GET.get('page_twitter')
+        self.page_wikipedia = request.GET.get('page_wikipedia')
         form_class = self.get_form_class()
         form = self.get_form(form_class)
-        context = {}
         if 'query' in request.GET:
             query = request.GET.get('query')
             form = SearchForm(request.GET)
+            self.context['query'] = query
             if 'search_criteria' in request.GET:
                 sc = request.GET.getlist('search_criteria')
-                self.get_data(query, context, 'T' in sc, 'W' in sc)
-        context['form'] = form
-        return self.render_to_response(self.get_context_data(**context))
+                self.get_data(query, 'T' in sc, 'W' in sc)
+                #context['twitter_results'] = self.get_page(page, context['twitter_results'])
+                #context['wikipedia_results'] = self.get_page(page, context['wikipedia_results'])
+        self.context['form'] = form
+        return self.render_to_response(self.get_context_data(**self.context))
 
-    def get_data(self, query, context, search_twitter, search_wikipedia):
+    def get_page(self, page, objects):
+        paginator = Paginator(objects, 10)
+        try:
+            objects = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            objects = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            objects = paginator.page(paginator.num_pages)
+        return objects
+
+    def get_data(self, query, search_twitter, search_wikipedia):
         if search_twitter:
-            self.get_twitter_data(query, context)
+            self.get_twitter_data(query)
         if search_wikipedia:
-            self.get_wikipedia_data(query, context)
+            self.get_wikipedia_data(query)
 
-    def get_twitter_data(self, query, context):
+    def get_twitter_data(self, query):
+        self.context['sct'] = True
+        self.context['page_twitter'] = self.page_twitter
         twitter_api = twython.Twython(settings.TWITTER_API_KEY, access_token=settings.TWITTER_ACCESS_TOKEN)
         try:
-            context['twitter_results'] = twitter_api.search(q=query)['statuses']
+            self.context['twitter_results'] = twitter_api.search(q=query, count=100)['statuses']
+            self.context['twitter_results'] = self.get_page(self.page_twitter, self.context['twitter_results'])
         except twython.TwythonAuthError:
-            context['twitter_error'] = 'Twitter API authentification error'
+            self.context['twitter_error'] = 'Twitter API authentification error'
         except twython.TwythonRateLimitError:
-            context['twitter_error'] = 'Rate limit error: Please wait before searching again'
+            self.context['twitter_error'] = 'Rate limit error: Please wait before searching again'
         except twython.TwythonError:
-            context['twitter_error'] = 'Connection Error'
+            self.context['twitter_error'] = 'Connection Error'
 
-    def get_wikipedia_data(self, query, context):
-        # Search wikipedia
+    def get_wikipedia_data(self, query):
+        self.context['scw'] = True
+        self.context['page_wikipedia'] = self.page_wikipedia
         try:
-            context['wikipedia_results'] = wikipedia.search(query, results=15)
+            self.context['wikipedia_results'] = wikipedia.search(query, results=100)
+            self.context['wikipedia_results'] = self.get_page(self.page_wikipedia, self.context['wikipedia_results'])
         except (wikipedia.HTTPTimeoutError, ConnectionError):
-            context['wikipedia_error'] = "Wikipedia is not responding"
+            self.context['wikipedia_error'] = "Wikipedia is not responding"
